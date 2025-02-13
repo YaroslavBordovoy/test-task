62974cf7c9c7ed86fa3a664eff9f5c5d8bbe59b2
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-02-13 20:18:06 +0200

    feat: Add sqlalchemy, create PostModel

---
+++ api_fetcher/database/models/posts.py
@@ -0,0 +1,19 @@
+from api_fetcher.database.models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String
+
+
+class PostModel(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    title: Mapped[str] = mapped_column(String(127), nullable=False)
+    body: Mapped[str] = mapped_column(String(511), nullable=False)
+
+    def __repr__(self):
+        return f"<PostModel(id={self.id}, user_id={self.user_id}, title={self.title}, body={self.body[:50]})>"
+
+    @classmethod
+    def default_order_by(cls):
+        return cls.id.asc()
