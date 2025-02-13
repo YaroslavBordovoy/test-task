b829d131fec037efb8b0e5940a8902fa05d6826d
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-02-13 21:31:23 +0200

    feat: Add settings for db

---
+++ api_fetcher/database/settings.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+
+SQLITE_DATABASE_URL = "sqlite:///./posts.db"
+
+sqlite_engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
+sqlite_connection = sqlite_engine.connect()
+SqliteSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sqlite_connection)
+
+
+def get_db() -> Session:
+    db = SqliteSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
