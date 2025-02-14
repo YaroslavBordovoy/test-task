41c23bdec809d17c9b2bb681dea8983f510352e0
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-02-14 14:45:22 +0200

    feat: Add logging

--- api_fetcher/fetch_data.py
+++ api_fetcher/fetch_data.py
@@ -1,7 +1,9 @@
+import asyncio
 import json
 
 import aiohttp
-import asyncio
+from pydantic import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
 
 from api_fetcher.database.models.posts import PostModel
 from api_fetcher.exceptions import (
@@ -11,9 +13,8 @@ from api_fetcher.exceptions import (
     APIDataError,
 )
 from api_fetcher.schemas import PostSchema
-from pydantic import ValidationError
 from api_fetcher.storages import write_to_db, write_to_csv
-from sqlalchemy.exc import SQLAlchemyError
+from api_fetcher.logging import logger
 
 
 JSONPLACEHOLDER_URL = "https://jsonplaceholder.typicode.com/posts"
@@ -51,20 +52,26 @@ def create_post_instances(data: list[dict]) -> list[PostModel]:
 
 
 async def get_posts(session: aiohttp.ClientSession) -> list[dict]:
+    logger.info(f"Sending API request: {JSONPLACEHOLDER_URL}.")
     try:
         async with session.get(JSONPLACEHOLDER_URL) as response:
             if response.status == 200:
                 try:
                     json_response = await response.json()
+                    logger.info(f"Successful API response: {len(json_response)} records received.")
 
                     return json_response
                 except json.JSONDecodeError:
+                    logger.error("Error processing json response.", exc_info=True)
                     raise APIDataError()
 
+            logger.warning(f"API returned error {response.status}")
             raise APIResponseError(status_code=response.status)
     except aiohttp.ClientConnectionError:
+        logger.error("Error connecting to API.", exc_info=True)
         raise APIConnectionError()
     except asyncio.TimeoutError:
+        logger.error("Response timeout exceeded.", exc_info=True)
         raise APITimeoutError()
 
 
