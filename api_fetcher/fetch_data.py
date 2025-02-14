2ec4e0bbef1d908fadc4bb3b7f254b03ebca3ba2
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-02-14 12:15:33 +0200

    feat: Upgrade PostSchema, add create_post_instances and validate_json to fetch_data.py

--- api_fetcher/fetch_data.py
+++ api_fetcher/fetch_data.py
@@ -3,15 +3,49 @@ import json
 import aiohttp
 import asyncio
 
+from api_fetcher.database.models.posts import PostModel
 from api_fetcher.exceptions import (
     APITimeoutError,
     APIConnectionError,
     APIResponseError,
     APIDataError,
 )
+from api_fetcher.schemas import PostSchema
+from pydantic import ValidationError
 
 
 JSONPLACEHOLDER_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+def validate_json(data: list[dict]) -> list[PostSchema]:
+    validated_posts = []
+
+    for post in data:
+        try:
+            validated_posts.append(PostSchema.model_validate(post))
+
+        except ValidationError:
+            raise ValidationError("An error occurred while validating json.")
+
+    return validated_posts
+
+
+def create_post_instances(data: list[dict]) -> list[PostModel]:
+    validated_posts = validate_json(data)
+
+    if not validated_posts:
+        return []
+
+    posts = [
+        PostModel(
+            user_id=schema.user_id,
+            title=schema.title,
+            body=schema.body
+        )
+        for schema in validated_posts
+    ]
+
+    return posts
 
 
 async def get_posts(session: aiohttp.ClientSession) -> list[dict]:
@@ -35,6 +69,7 @@ async def get_posts(session: aiohttp.ClientSession) -> list[dict]:
 async def main():
     async with aiohttp.ClientSession() as session:
         posts = await get_posts(session)
+        data_for_db = create_post_instances(posts)
 
 
 if __name__ == "__main__":
--- api_fetcher/schemas.py
+++ api_fetcher/schemas.py
@@ -1,10 +1,12 @@
 from typing import Annotated
 
 from annotated_types import Gt, MinLen, MaxLen
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class PostSchema(BaseModel):
     user_id: Annotated[int, Gt(0)]
     title: Annotated[str, MinLen(1), MaxLen(127)]
     body: Annotated[str, MinLen(1), MaxLen(511)]
+
+    model_config = ConfigDict(extra="ignore")
