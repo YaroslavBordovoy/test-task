4cbba2f2ba5458d3078b2130008ae6b4c784de2a
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-02-16 12:44:52 +0200

    Add documentaion to task_manager

--- task_manager/manager.py
+++ task_manager/manager.py
@@ -10,6 +10,21 @@ from task_manager.database.settings import get_db
 
 
 class TaskManager:
+    """
+    A class for managing tasks in the database.
+
+    This class provides methods for adding, updating, retrieving, and deleting tasks.
+    It interacts with the database via SQLAlchemy and logs all operations.
+
+    Attributes:
+        db (Session): The database session used for executing queries.
+
+    Methods:
+        add_task(title, description, due_date): Adds a new task to the database.
+        update_task_status(status, task_id=None, title=None): Updates the status of a task.
+        task_list(): Retrieves all tasks ordered by due date.
+        delete_task(task_id=None, title=None): Deletes a task from the database.
+    """
 
     def __init__(self):
         self.db = next(get_db())
@@ -29,6 +44,17 @@ class TaskManager:
         return task
 
     def add_task(self, title: str, description: str, due_date: str) -> None:
+        """
+        Adds a new task to the database.
+
+        Args:
+            title (str): The title of the task.
+            description (str): A brief description of the task.
+            due_date (str): The due date in the format "DD-MM-YYYY".
+
+        Raises:
+            ValueError: If a task with the same title already exists.
+        """
         title_exist = self.db.scalar(select(TaskManagerModel).where(TaskManagerModel.title == title))
 
         if title_exist:
@@ -51,6 +77,14 @@ class TaskManager:
         )
 
     def update_task_status(self, status: str, task_id: int = None, title: str = None) -> None:
+        """
+        Update the task.
+
+        Args:
+            status (str): New status for the task.
+            task_id (Optional[int]): The ID of the task.
+            title (Optional[str]): The title of the task.
+        """
         if status not in TaskStatus.__members__.values():
             raise ValueError("Invalid status.")
 
@@ -65,9 +99,25 @@ class TaskManager:
         )
 
     def task_list(self) -> None:
+        """
+        Outputs a list of tasks to the terminal.
+
+        Returns:
+            None
+        """
         logger.info(list(self.db.scalars(select(TaskManagerModel).order_by(TaskManagerModel.due_date))))
 
     def delete_task(self, task_id: int = None, title: str = None) -> None:
+        """
+        Delete the task.
+
+        Args:
+            task_id (Optional[int]): The ID of the task.
+            title (Optional[str]): The title of the task.
+
+        Returns:
+            None
+        """
         task_to_delete = self.check_for_argument_existence(self.db, task_id, title)
 
         self.db.delete(task_to_delete)
